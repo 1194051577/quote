f4651877a415ce31dee441ba48dce37d38b1651c
zhouww <zhouww@servyou.com.cn>
2021-03-14 20:09:21 +0800

    增加复权条件

--- com/zhouww/easyMoney/KLine.py
+++ com/zhouww/easyMoney/KLine.py
@@ -180,6 +180,8 @@ kline = KLine()
 # print(kline.downloadDay(companyCode="600460", yearMonthDay="20210203"))
 # print(kline.downloadMonth(companyCode="600460", yearMonth="202103"))
 # print(kline.downloadYear(companyCode="600460", year="2021"))
+print(kline.downloadDayRange(companyCode="600460", startYearMonthDay="20210205", endYearMonthDay="20210312", fqType="不复权"))
+print(kline.downloadDayRange(companyCode="600460", startYearMonthDay="20210205", endYearMonthDay="20210312", fqType="前复权"))
 print(kline.downloadDayRange(companyCode="600460", startYearMonthDay="20210205", endYearMonthDay="20210312", fqType="后复权"))
 
 
